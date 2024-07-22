a7293fb477bafaf27df41fe655a856a55e7638d8
a12bb <2713204748@qq.com>
2024-07-22 13:13:40 +0800

    optimize:修改sql文件，优化sql语句

---
+++ big-market-domain/src/main/java/com/big/market/infrastructure/domain/strategy/model/valobj/RuleTreeVO.java
@@ -0,0 +1,33 @@
+package com.big.market.infrastructure.domain.strategy.model.valobj;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * @author LYT0905
+ * @Description: 规则树对象【注意；不具有唯一ID，不需要改变数据库结果的对象，可以被定义为值对象】
+ * @Date: 2024/07/22 12:42:47
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RuleTreeVO {
+
+    /** 规则树ID */
+    private Integer treeId;
+    /** 规则树名称 */
+    private String treeName;
+    /** 规则树描述 */
+    private String treeDesc;
+    /** 规则根节点 */
+    private String treeRootRuleNode;
+
+    /** 规则节点 */
+    private Map<String, RuleTreeNodeVO> treeNodeMap;
+
+}
--- docs/dev-ops/environment/mysql/sql/big-market.sql
+++ docs/dev-ops/environment/mysql/sql/big-market.sql
@@ -2,6 +2,7 @@ create table strategy(
                          id bigint(11) unsigned not null auto_increment comment '自增ID',
                          strategy_id bigint(8) not null comment '抽奖策略ID',
                          strategy_desc varchar(128) not null comment '抽奖策略描述',
+                         rule_models varchar(255) default null comment '规则模型',
                          create_time datetime not null default CURRENT_TIMESTAMP comment '创建时间',
                          update_time datetime not null default CURRENT_TIMESTAMP on update current_timestamp comment '更新时间',
                          primary key (id),
@@ -11,6 +12,12 @@ insert into strategy
 (id, strategy_id, strategy_desc) values
     (null, 100001, '抽奖策略');
 
+
+INSERT INTO `strategy` (`id`, `strategy_id`, `strategy_desc`, `rule_models`)
+VALUES
+    (2,100003,'抽奖策略-验证lock',NULL),
+    (3,100002,'抽奖策略-非完整1概率',NULL);
+
 create table strategy_award(
                                id bigint(11) unsigned not null auto_increment comment '自增ID',
                                strategy_id bigint(8) not null comment '抽奖策略ID',
@@ -19,7 +26,7 @@ create table strategy_award(
                                award_subtitle varchar(128) default null comment '抽奖奖品副标题',
                                award_count int(8) not null default '0' comment '奖品库存总量',
                                award_count_surplus int(8) not null default '0' comment '奖品库存剩余',
-                               award_rate decimal(6, 4) not null comment '奖品中奖概率',
+                               award_rate decimal(6, 2) not null comment '奖品中奖概率',
                                rule_models varchar(256) default null comment '规则模型，rule配置的模型同步到此表，便于使用',
                                `sort` int(2) NOT NULL DEFAULT '0' COMMENT '排序',
                                create_time datetime not null default CURRENT_TIMESTAMP comment '创建时间',
@@ -27,7 +34,7 @@ create table strategy_award(
                                primary key (id),
                                key idx_strategy_id_award_id(strategy_id, award_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-LOCK TABLES `strategy_award` WRITE;
+
 /*!40000 ALTER TABLE `strategy_award` DISABLE KEYS */;
 
 INSERT INTO `strategy_award` (`id`, `strategy_id`, `award_id`, `award_title`, `award_subtitle`, `award_count`, `award_count_surplus`, `award_rate`, `rule_models`, `sort`)
@@ -42,6 +49,15 @@ VALUES
     (8,100001,108,'增加100次使用','抽奖2次后解锁',199,199,0.1999,'rule_lock,rule_luck_award',8),
     (9,100001,109,'解锁全部模型','抽奖6次后解锁',1,1,0.0001,'rule_lock,rule_luck_award',9);
 
+INSERT INTO `strategy_award` (`id`, `strategy_id`, `award_id`, `award_title`, `award_subtitle`, `award_count`, `award_count_surplus`, `award_rate`, `rule_models`, `sort`)
+VALUES
+    (10,100002,101,'随机积分',NULL,1,1,0.5000,'rule_random,rule_luck_award',1),
+    (11,100002,102,'5次使用',NULL,1,1,0.1000,'rule_random,rule_luck_award',2),
+    (12,100002,106,'增加dall-e-2画图模型',NULL,1,1,0.10,'rule_random,rule_luck_award',3),
+    (13,100003,107,'增加dall-e-3画图模型','抽奖1次后解锁',200,200,0.04,'rule_lock,rule_luck_award',7),
+    (14,100003,108,'增加100次使用','抽奖2次后解锁',199,199,0.09,'rule_lock,rule_luck_award',8),
+    (15,100003,109,'解锁全部模型','抽奖6次后解锁',1,1,0.01,'rule_lock,rule_luck_award',9);
+
 create table strategy_rule(
                               id bigint(11) unsigned not null auto_increment comment '自增ID',
                               strategy_id bigint(8) not null comment '抽奖策略ID',
@@ -55,7 +71,6 @@ create table strategy_rule(
                               primary key (id),
                               key idx_strategy_id_award_id(strategy_id, award_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-LOCK TABLES `strategy_rule` WRITE;
 /*!40000 ALTER TABLE `strategy_rule` DISABLE KEYS */;
 
 INSERT INTO `strategy_rule` (`id`, `strategy_id`, `award_id`, `rule_type`, `rule_model`, `rule_value`, `rule_desc`)
@@ -75,6 +90,13 @@ VALUES
     (13,100001,NULL,1,'rule_weight','6000,102,103,104,105,106,107,108,109','消耗6000分，必中奖范围'),
     (14,100001,NULL,1,'rule_blacklist','1','黑名单抽奖，积分兜底');
 
+
+INSERT INTO `strategy_rule` (`id`, `strategy_id`, `award_id`, `rule_type`, `rule_model`, `rule_value`, `rule_desc`)
+VALUES
+    (15,100003,107,2,'rule_lock','1','抽奖1次后解锁'),
+    (16,100003,108,2,'rule_lock','2','抽奖2次后解锁'),
+    (17,100003,109,2,'rule_lock','6','抽奖6次后解锁');
+
 CREATE TABLE `award` (
                          `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
                          `award_id` int(8) NOT NULL COMMENT '抽奖奖品ID - 内部流转使用',
@@ -86,7 +108,7 @@ CREATE TABLE `award` (
                          PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-LOCK TABLES `award` WRITE;
+
 /*!40000 ALTER TABLE `award` DISABLE KEYS */;
 
 INSERT INTO `award` (`id`, `award_id`, `award_key`, `award_config`, `award_desc`, `create_time`, `update_time`)
@@ -100,4 +122,5 @@ VALUES
     (7,107,'openai_model','dall-e-3','OpenAI 增加模型','2023-12-09 11:07:06','2023-12-09 11:12:10'),
     (8,108,'openai_use_count','100','OpenAI 增加使用次数','2023-12-09 11:07:06','2023-12-09 11:12:55'),
     (9,109,'openai_model','gpt-4,dall-e-2,dall-e-3','OpenAI 增加模型','2023-12-09 11:07:06','2023-12-09 11:12:39');
+
 
